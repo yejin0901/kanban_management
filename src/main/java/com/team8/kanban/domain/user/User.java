091e51177cd2e2096728039d81b86bb5c3d245e0
Yejin <ggg06142@gmail.com>
2024-03-18 15:50:03 +0900

    initialize

---
+++ src/main/java/com/team8/kanban/domain/user/User.java
@@ -0,0 +1,25 @@
+package com.team8.kanban.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+
+    @Column(nullable = false)
+    private String password;
+}
