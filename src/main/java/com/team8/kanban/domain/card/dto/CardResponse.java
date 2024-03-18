793f48bdc748cba056059ec820968f36ae738e60
pshistory <psh_a@naver.com>
2024-03-18 21:50:53 +0900

    fix : 이용자에게 Card 전달할때 작성한 userName 전달하도록 코드 수정

--- src/main/java/com/team8/kanban/domain/card/dto/CardResponse.java
+++ src/main/java/com/team8/kanban/domain/card/dto/CardResponse.java
@@ -1,15 +1,23 @@
 package com.team8.kanban.domain.card.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+
+@Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CardResponse {
 
     private Long cardId;
     private String cardName;
     private String description;
+    private String userName;
     private LocalDateTime expiredDate;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
