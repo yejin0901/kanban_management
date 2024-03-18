c138b02a19cdc342043cdf0090414b07729cde63
Esunsin <jinchan1148@gmail.com>
2024-03-18 16:10:43 +0900

    :tada: init

--- src/main/java/com/team8/kanban/global/security/UserDetailsServiceImpl.java
+++ src/main/java/com/team8/kanban/global/security/UserDetailsServiceImpl.java
@@ -1,30 +1,26 @@
 package com.team8.kanban.global.security;
 
 
+import com.team8.kanban.domain.user.User;
+import com.team8.kanban.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import study.kfarm.user.User;
-import study.kfarm.user.UserRepository;
+
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
-
-        return new UserDetailsImpl(user);
+        return null;
     }
-    public UserDetailsImpl getUserDetails(String username) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Not Found" + username));
+    public UserDetailsImpl getUserDetails(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username).orElseThrow();
         return new UserDetailsImpl(user);
     }
 }
