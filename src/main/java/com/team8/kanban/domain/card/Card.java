abf6a205f2d1ff1ffcf87e3f7ed6d1f4b60b09b5
pshistory <psh_a@naver.com>
2024-03-18 21:47:24 +0900

    fix: 1. Card Entity User와 단방향연관관계 생성      2. Card Entity 만료일(expiredDate) LocalDate -> LocalDateTime 으로 타입 변경      3. Color enum값 기존 -> 신규생성한 ColorEnum 으로 필드 변경

--- src/main/java/com/team8/kanban/domain/card/Card.java
+++ src/main/java/com/team8/kanban/domain/card/Card.java
@@ -1,6 +1,8 @@
 package com.team8.kanban.domain.card;
 
 import com.team8.kanban.domain.card.dto.UpdateCardRequest;
+import com.team8.kanban.domain.user.User;
+import com.team8.kanban.global.entity.ColorEnum;
 import com.team8.kanban.global.entity.TimeStamped;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -8,8 +10,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.awt.*;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Builder
@@ -22,15 +23,23 @@ public class Card extends TimeStamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cardId;
+
     private String cardName;
+
     private String description;
-    private LocalDate expiredDate;
-    private Color color;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id")
+    private User user;
+
+    private LocalDateTime expiredDate;
+
+    private ColorEnum colorEnum;
 
     public void update(UpdateCardRequest request) {
         this.cardName = request.getCardName();
         this.description = request.getDescription();
         this.expiredDate = request.getExpiredDate();
-        this.color = Color.decode(request.getColor());
+        this.colorEnum = ColorEnum.valueOf(request.getColor());
     }
 }
