39ab2b7f5a0ca2cd9798b6516601aee4c0d83065
Esunsin <jinchan1148@gmail.com>
2024-03-19 19:31:11 +0900

    :sparkles: feat : global Exception 추가(IllegalException, nullpointerException, DuplicateKeyException)

--- src/main/java/com/team8/kanban/global/exception/GlobalControllerAdvice.java
+++ src/main/java/com/team8/kanban/global/exception/GlobalControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.team8.kanban.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalControllerAdvice {
     @ExceptionHandler({ChangeSetPersister.NotFoundException.class, IllegalAccessException.class})
     public ResponseEntity<CommonResponse<?>> handleException(IllegalArgumentException ex) {
@@ -15,5 +18,42 @@ public class GlobalControllerAdvice {
                 Exception,
                 HttpStatus.BAD_REQUEST
         );
+    }
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<CommonResponse<ExceptionResponse>> IllegalArgumentException(IllegalArgumentException ex) {
+        log.info(">>>IllegalArgumentException<<<");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(CommonResponse.<ExceptionResponse>builder()
+                        .msg(ex.getMessage())
+                        .data(createResponse(HttpStatus.BAD_REQUEST))
+                        .build());
+
+    }
+    @ExceptionHandler({NullPointerException.class})
+    public ResponseEntity<CommonResponse<ExceptionResponse>> NullPointerException(NullPointerException ex) {
+        log.info(">>>NullPointerException<<<");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(CommonResponse.<ExceptionResponse>builder()
+                        .msg(ex.getMessage())
+                        .data(createResponse(HttpStatus.BAD_REQUEST))
+                        .build());
+
+    }
+    @ExceptionHandler({DuplicateKeyException.class})
+    public ResponseEntity<CommonResponse<ExceptionResponse>> DuplicateKeyException(DuplicateKeyException ex) {
+        log.info(">>>DuplicateKeyException<<<");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(CommonResponse.<ExceptionResponse>builder()
+                        .msg(ex.getMessage())
+                        .data(createResponse(HttpStatus.BAD_REQUEST))
+                        .build());
+
+    }
+
+    private ExceptionResponse createResponse(HttpStatus status) {
+        return ExceptionResponse.builder()
+                .statusCode(status.value())
+                .state(status)
+                .build();
     }
 }
