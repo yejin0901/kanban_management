bf0cfb1ef7cb3798b7bebd389c200b30b389ffe4
pshistory <psh_a@naver.com>
2024-03-19 12:59:34 +0900

    fix : 테스트코드 작성위해 GlobalControllerAdvice에 IllegalArgumentException 추가 및 User Entity @builder 추가

--- src/main/java/com/team8/kanban/global/exception/GlobalControllerAdvice.java
+++ src/main/java/com/team8/kanban/global/exception/GlobalControllerAdvice.java
@@ -1,11 +1,14 @@
 package com.team8.kanban.global.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerAdvice {
     @ExceptionHandler({ChangeSetPersister.NotFoundException.class, IllegalAccessException.class})
@@ -15,5 +18,11 @@ public class GlobalControllerAdvice {
                 Exception,
                 HttpStatus.BAD_REQUEST
         );
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<CommonResponse<String>> handleIllegalArgumentExceptions(IllegalArgumentException ex, HttpServletRequest request) {
+        log.error("url: {}, 메세지: {} \n stacktrace: {}", request.getRequestURI(), ex.getMessage(), ex.fillInStackTrace());
+        return ResponseEntity.badRequest().body(new CommonResponse<String>(ex.getMessage()));
     }
 }
