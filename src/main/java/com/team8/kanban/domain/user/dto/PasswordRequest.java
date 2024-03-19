2c0905fa01e3f6d7ad531a07e70fa07846aefd34
Esunsin <jinchan1148@gmail.com>
2024-03-19 20:09:07 +0900

    :sparkles: feat : test를 위한 전체 생성자 추가

--- src/main/java/com/team8/kanban/domain/user/dto/PasswordRequest.java
+++ src/main/java/com/team8/kanban/domain/user/dto/PasswordRequest.java
@@ -1,8 +1,10 @@
 package com.team8.kanban.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class PasswordRequest {
     private String oldPassword;
     private String newPassword;
