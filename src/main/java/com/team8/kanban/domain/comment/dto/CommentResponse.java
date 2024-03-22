8be700b5528092feda304a1cca4be3c923ace323
Esunsin <jinchan1148@gmail.com>
2024-03-22 14:04:33 +0900

    :truck: fix : rename field

--- src/main/java/com/team8/kanban/domain/comment/dto/CommentResponse.java
+++ src/main/java/com/team8/kanban/domain/comment/dto/CommentResponse.java
@@ -3,23 +3,25 @@ package com.team8.kanban.domain.comment.dto;
 import com.team8.kanban.domain.comment.Comment;
 import com.team8.kanban.domain.user.User;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 public class CommentResponse {
 
     private Long id;
     private String content;
     private String writer;
-    private LocalDateTime createAt;
-    private LocalDateTime updateAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
-    public CommentResponse(Comment comment , User user) {
+    public CommentResponse(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
-        this.writer = user.getUsername();
-        this.createAt = comment.getCreatedAt();
-        this.updateAt = comment.getModifiedAt();
+        this.writer = comment.getUser().getUsername();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
     }
 }
